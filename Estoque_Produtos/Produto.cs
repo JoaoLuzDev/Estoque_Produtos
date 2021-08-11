22045185c2f6d59edfabd9da8a0042c6c91b34cb
jluz <jluz@I4PRONOTE19>
2021-08-11 10:10:17 -0300

    Add project files.

---
+++ Estoque_Produtos/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estoque_Produtos
+{
+    class Produto
+    {
+
+        public string Nome;
+        public int Quantidade;
+        public double Preco;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+    }
+}
---
+++ Estoque_Produtos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estoque_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto Prod;
+            Prod = new Produto();
+
+            Console.WriteLine(" Ente com os dados do produto: ");
+            Console.WriteLine(" Nome: ");
+            Prod.Nome = Console.ReadLine();
+            
+            Console.WriteLine(" Preço: ");
+            Prod.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.WriteLine(" Quantidade no estoque: ");
+            Prod.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(" Dados do produto: " + Prod.Nome + ", $ " + Prod.Preco + ", " + Prod.Quantidade + " unidades, Total: $ " );
+
+            Console.WriteLine(" Digite o Número de produtos a ser adicionado: ");
+            Prod.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        }
+    }
+}
